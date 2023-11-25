3a17283d27b18b0da36b888b2877d405d3153530
DanielH <dhernandezq2@miumg.edu.gt>
2023-11-24 19:31:57 -0600

    implementacion lab y sucursal

--- src/app/laboratorio/laboratorio/laboratorio.component.ts
+++ src/app/laboratorio/laboratorio/laboratorio.component.ts
@@ -1,4 +1,8 @@
 import { Component, OnInit } from '@angular/core';
+import { HttpClient, HttpHeaders } from '@angular/common/http';
+import {catchError, timestamp} from 'rxjs/operators';
+import { Observable } from 'rxjs';
+import { ThisReceiver } from '@angular/compiler';
 
 @Component({
   selector: 'app-laboratorio',
@@ -6,10 +10,78 @@ import { Component, OnInit } from '@angular/core';
   styleUrls: ['./laboratorio.component.css']
 })
 export class LaboratorioComponent implements OnInit {
+  laboratorio:any ={};
+  laboratorios:any ={};
 
-  constructor() { }
+  constructor(private http:HttpClient) { }
 
   ngOnInit(): void {
+    this.buscarLaboratorio();
   }
 
+  buscarLaboratorio(){
+    this.buscarLaboratorioServicio().subscribe(
+      (response:any) => this.laboratorios = response
+    )
+  }
+
+  buscarLaboratorioServicio():Observable<any>{
+    return this.http.get<any>("http://localhost:8080/proyecto/blaboratorio")
+    .pipe(
+      catchError(e => "error")
+    );
+  }
+
+  crearLaboratorio(){
+    let formFormulario:any = document.getElementById("formLaboratorio");
+    let formValido:boolean = formFormulario.reportValidity();
+    if(formValido){
+      this.crearLaboratorioServicio().subscribe(
+        (response:any) => this.actualizar(response)
+      )
+    }
+  }
+
+  crearLaboratorioServicio(){
+    var httpOptions = {
+      headers: new HttpHeaders({
+        'Content-Type':'application/json'
+      })
+    }
+    return this.http.post<any>("http://localhost:8080/proyecto/alaboratorio",this.laboratorio,httpOptions)
+    .pipe(
+      catchError(e =>"error")
+    );
+  }
+
+  actualizar(laboratorio:any){
+    alert("Laboratorio guardado exitosamente con el id: "+ laboratorio.codigolaboratorio);
+    this.buscarLaboratorio();
+    this.laboratorio = {};
+  }
+
+  Limpiar(){
+    this.laboratorio = {};
+  }
+
+  modificar(u:any){
+    this.laboratorio =u;
+  }
+
+  Eliminar(u:any){
+    this.eliminarLaboratorioServicio(u.codigolaboratorio).subscribe(
+      (response:any) => this.actualizarEliminar(response)
+    )
+  }
+
+  eliminarLaboratorioServicio(codigolaboratorio:any){
+    return this.http.delete<any>("http://localhost:8080/proyecto/eliminarlaboratorio/"+codigolaboratorio)
+    .pipe(
+      catchError(e =>"error")
+    );
+  }
+  actualizarEliminar(response:any){
+    alert("Laboratorio eliminada exitosamente");
+    this.buscarLaboratorio();
+  }
 }
--- src/app/sucursal/sucursal/sucursal.component.ts
+++ src/app/sucursal/sucursal/sucursal.component.ts
@@ -1,4 +1,8 @@
 import { Component, OnInit } from '@angular/core';
+import { HttpClient, HttpHeaders } from '@angular/common/http';
+import {catchError, timestamp} from 'rxjs/operators';
+import { Observable } from 'rxjs';
+import { ThisReceiver } from '@angular/compiler';
 
 @Component({
   selector: 'app-sucursal',
@@ -6,10 +10,78 @@ import { Component, OnInit } from '@angular/core';
   styleUrls: ['./sucursal.component.css']
 })
 export class SucursalComponent implements OnInit {
+  sucursal:any ={};
+  sucursales:any ={};
 
-  constructor() { }
+  constructor(private http:HttpClient) { }
 
   ngOnInit(): void {
+    this.buscarSucursal();
   }
 
+  buscarSucursal(){
+    this.buscarSucursalServicio().subscribe(
+      (response:any) => this.sucursales = response
+    )
+  }
+
+  buscarSucursalServicio():Observable<any>{
+    return this.http.get<any>("http://localhost:8080/proyecto/bsucursal")
+    .pipe(
+      catchError(e => "error")
+    );
+  }
+
+  crearSucursal(){
+    let formFormulario:any = document.getElementById("formSucursal");
+    let formValido:boolean = formFormulario.reportValidity();
+    if(formValido){
+      this.crearSucursalServicio().subscribe(
+        (response:any) => this.actualizar(response)
+      )
+    }
+  }
+
+  crearSucursalServicio(){
+    var httpOptions = {
+      headers: new HttpHeaders({
+        'Content-Type':'application/json'
+      })
+    }
+    return this.http.post<any>("http://localhost:8080/proyecto/asucursal",this.sucursal,httpOptions)
+    .pipe(
+      catchError(e =>"error")
+    );
+  }
+
+  actualizar(sucursal:any){
+    alert("Sucursal guardado exitosamente con el id: "+ sucursal.codigosucursal);
+    this.buscarSucursal();
+    this.sucursal = {};
+  }
+
+  Limpiar(){
+    this.sucursal = {};
+  }
+
+  modificar(u:any){
+    this.sucursal =u;
+  }
+
+  Eliminar(u:any){
+    this.eliminarSucursalServicio(u.codigosucursal).subscribe(
+      (response:any) => this.actualizarEliminar(response)
+    )
+  }
+
+  eliminarSucursalServicio(codigosucursal:any){
+    return this.http.delete<any>("http://localhost:8080/proyecto/eliminarsucursal/"+codigosucursal)
+    .pipe(
+      catchError(e =>"error")
+    );
+  }
+  actualizarEliminar(response:any){
+    alert("Sucursal eliminada exitosamente");
+    this.buscarSucursal();
+  }
 }
